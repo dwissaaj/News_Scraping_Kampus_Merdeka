036af345fadb4288b0d677a3e57a1cb923d9252d
dwissaaj <dwis.buntelman123@gmail.com>
2021-10-10 20:02:05 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import newspaper
+from newspaper import Article
+df = pd.read_excel('Untitled 1.xlsx')
+urls = df['data'].to_list()
+
+
+for url in urls:
+    try:
+        a = Article(url, language='id')
+        a.download()
+        a.parse()
+
+        author = a.authors
+        dates = a.publish_date
+        add_data = a.additional_data
+        text = a.text
+        tag = a.tags
+        title = a.title
+        keywords = a.keywords
+
+        print(author,dates,add_data,text,tag,title,keywords)
+    except Exception as e:
+        print(e)
+
+
+'''
+a = Article(cek,language='id')
+b = a.download()
+c = a.parse()
+author = a.authors
+date = a.publish_date
+add_data = a.additional_data
+text = a.text
+tag = a.tags
+title = a.title
+keywords = a.keywords
+combine = {'author':author,'date':date,'text':
+text,'add_data':add_data,'tag':tag,'tittle':title,'keyword':keywords}
+data = pd.DataFrame(data=combine)'''
